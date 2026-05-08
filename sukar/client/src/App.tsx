02a69857448f3e6f33d179d11c11b8a580f003d8
ahmeez92-wq <ahmeez92@gmail.com>
2026-05-08 23:14:22 +0300

    Update App.tsx

--- sukar/client/src/App.tsx
+++ sukar/client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
@@ -14,11 +12,7 @@ function Router() {
 }
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  );
+  return <Router />;
 }
 
 export default App;
